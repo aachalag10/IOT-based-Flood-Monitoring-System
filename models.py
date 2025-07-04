c9e8219e909db0307b873d2b918c263f1ee0ce7d
aachalag10 <Aachalag75@gmail.com>
2025-07-04 12:06:43 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class registertable(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.EmailField()
+    phone=models.BigIntegerField()
+    password=models.CharField(max_length=8)
+
+# Create your models here.
